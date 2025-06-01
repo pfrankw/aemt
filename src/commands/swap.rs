5030d15305db04b3dd5a7f4be8d2f84330a6515b
Francesco Pompo <pfrankw@users.noreply.github.com>
2025-06-01 17:28:18 +0200

    Comment for swap

--- src/commands/swap.rs
+++ src/commands/swap.rs
@@ -3,6 +3,10 @@ use crate::{
     kidz::Kidz,
 };
 
+/// Implemented only for testing the boundaries of the KKIIDDZZ.DAT file.
+/// It's useless for modding as the only things it does is swapping two files inside the archive.
+/// Swapping seems to be permitted but not for all files. It's like some file offsets are hardcoded
+/// somewhere.
 pub fn swap(args: &Args, eargs: &SwapArgs) -> Result<(), crate::error::Error> {
     let mut kidz = Kidz::load(&args.hed, &args.dat, &args.bns)?;
 
